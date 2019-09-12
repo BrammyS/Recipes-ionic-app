d46f7037169bdac52553aa53f7697dd82a45105b
Bram Esendam <bram.esendam@gmail.com>
2019-09-12 10:39:05 +0200

    Added recipe service

--- Recipes/src/app/recipes/recipes.page.ts
+++ Recipes/src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesPage implements OnInit {
 
-  constructor() { }
+  constructor(private recipeService : RecipeService) { 
+
+  }
 
   ngOnInit() {
   }
+
+
 
 }
