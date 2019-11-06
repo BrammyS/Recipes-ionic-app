63fe9d9b93b674fd533a989269683ef01c21ae1f
Bram Esendam <bram.esendam@gmail.com>
2019-11-06 14:36:39 +0100

    Added recipes details

--- Recipes/src/app/recipes/details/details.page.ts
+++ Recipes/src/app/recipes/details/details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, NavController } from '@ionic/angular';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  recipe: {
+    index: number,
+    name: string,
+    imgUrl: string,
+    ingredients: Array<string>
+  };
+
+  constructor(private route: ActivatedRoute, public alertController: AlertController,
+              private recipeService: RecipeService, private navCtrl: NavController) {
+    this.route.queryParams.subscribe(params => {
+      if (params && params.special) {
+        this.recipe = JSON.parse(params.special);
+      }
+    });
+   }
+
+   async removeItem(index) {
+    const alert = await this.alertController.create({
+      header: 'Please confirm',
+      message: 'Do you want to remove ' + this.recipe.name + ' from the list?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+        }, {
+          text: 'Confirm',
+          handler: () => {
+            this.recipeService.removeRecipe(index);
+            this.navCtrl.navigateRoot('/');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
   ngOnInit() {
   }
-
 }
