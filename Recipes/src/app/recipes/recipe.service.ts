699baf0a076a1f4554403844968f09578bb66a9c
BramEsendam <bram.esendam@gmail.com>
2019-11-07 19:51:25 +0100

    Fixed null error on first start

--- Recipes/src/app/recipes/recipe.service.ts
+++ Recipes/src/app/recipes/recipe.service.ts
@@ -9,12 +9,12 @@ import { Storage } from '@ionic/storage';
 export class RecipeService {
 
   private iterableDiffer: any;
-  public recipes : Array<
-  {
-    name: string, 
-    imgUrl: string, 
-    ingredients: Array<string>
-  }>;
+  public recipes: Array<
+    {
+      name: string,
+      imgUrl: string,
+      ingredients: Array<string>
+    }>;
 
   constructor(private storage: Storage, private iterableDiffers: IterableDiffers) {
     this.recipes = new Array<{
@@ -24,37 +24,16 @@ export class RecipeService {
     }>();
 
     storage.get('recipes').then((data) => {
-      console.log(data);
+      if (data == null) {
+        data = new Array<{
+          name: string,
+          imgUrl: string,
+          ingredients: Array<string>
+        }>();
+      }
       this.recipes = data;
     });
     this.iterableDiffer = this.iterableDiffers.find([]).create(null);
-
-
-
-    // this.recipes.push({
-    //   name: "Pizza Margherita", 
-    //   imgUrl: "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--51643_11.jpg?itok=I_hF8vFL", 
-    //   ingredients: new Array<string>(
-    //     "300g strong bread flour",
-    //     "1 tsp instant yeast",
-    //     "1 tsp salt",
-    //     "1 tbsp olive oil",
-    //     "Tomato sauce"
-    //     )
-    // });
-    // this.recipes.push({
-    //   name: "Gennaro's classic spaghetti carbonara", 
-    //   imgUrl: "https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/xtra_med/1558_1_1436795948.jpg?tr=w-400", 
-    //   ingredients: new Array<string>(
-    //     "3 large free-range egg yolks",
-    //     "40 g Parmesan cheese",
-    //     "1 x 150 g piece of higher-welfare pancetta",
-    //     "200 g dried spaghetti",
-    //     "1 clove of garlic",
-    //     "extra virgin olive oil"
-    //     )
-    // });
-    // this.storage.set('recipes', this.recipes);
   }
 
   ngDoCheck() {
@@ -65,12 +44,12 @@ export class RecipeService {
   }
 
   addRecipe(recipeName: string, img: string, ingredientsAr: Array<string>) {
-      this.recipes.push({
-        name: recipeName,
-        imgUrl: img,
-        ingredients: ingredientsAr
-      });
-      this.storage.set('recipes', this.recipes);
+    this.recipes.push({
+      name: recipeName,
+      imgUrl: img,
+      ingredients: ingredientsAr
+    });
+    this.storage.set('recipes', this.recipes);
   }
 
   removeRecipe(index: number) {
